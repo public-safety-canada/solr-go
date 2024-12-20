fd62880a0240e3ce3a275bf18d64505058624820
Kevin Shelaga <kevin.shelaga@solo.io>
2024-12-20 07:29:09 -0500

    fix broken parsers

--- query_test.go
+++ query_test.go
@@ -60,7 +60,7 @@ func TestQuery(t *testing.T) {
 				{"#color_tag": solr.M{"field": solr.M{"f": "color", "query": "Red"}}},
 			},
 		},
-		"query": "{!dismax}'solr rocks'",
+		"query": "{!dismax v='solr rocks'}",
 		"sort":  "score",
 	}
 
